7aa248d25efbc932d5c5a83096aade8b56debf92
Eric Batista <klose.eric31@gmail.com>
2021-01-12 01:02:02 -0300

    Create a startGame() and updateSnake() functions

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'dart:async';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,11 +10,33 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   static List<int> snakePosition = [45, 65, 85, 105, 125];
   int numberOfSquares = 760;
-
   int food = Random().nextInt(700);
+  var direction = 'down';
 
   void generateFood() {
     food = Random().nextInt(700);
+  }
+
+  void startGame() {
+    const duration = Duration(milliseconds: 300);
+    Timer.periodic(duration, (Timer timer) {
+      updateSnake();
+    });
+  }
+
+  void updateSnake() {
+    setState(() {
+      switch (direction) {
+        case 'down':
+          if (snakePosition.last > 740) {
+            snakePosition.add(snakePosition.last + 20 - 760);
+          } else {
+            snakePosition.add(snakePosition.last + 20);
+          }
+          break;
+        default:
+      }
+    });
   }
 
   @override
@@ -24,6 +47,20 @@ class _HomePageState extends State<HomePage> {
         children: [
           Expanded(
             child: GestureDetector(
+              // onVerticalDragUpdate: (details) {
+              //   if (direction != 'up' && details.delta.dy > 0) {
+              //     direction = 'down';
+              //   } else if (direction != 'down' && details.delta.dy < 0) {
+              //     direction = 'up';
+              //   }
+              // },
+              // onHorizontalDragUpdate: (details) {
+              //   if (direction != 'left' && details.delta.dx > 0) {
+              //     direction = 'right';
+              //   } else if (direction != 'right' && details.delta.dx < 0) {
+              //     direction = 'left';
+              //   }
+              // },
               child: Container(
                 child: GridView.builder(
                   physics: NeverScrollableScrollPhysics(),
